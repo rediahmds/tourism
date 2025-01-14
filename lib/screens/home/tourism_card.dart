97a291d30d86644c17b84e55f0cf22871e19e289
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-01-14 22:27:39 +0700

    feat(home): :sparkles: wrap card with inkwell
    enables ontap action

--- lib/screens/home/tourism_card.dart
+++ lib/screens/home/tourism_card.dart
@@ -8,30 +8,43 @@ class TourismCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 16,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            tourism.name,
-            style: const TextStyle(
-              fontSize: 16,
-            ),
+    return InkWell(
+      onTap: _goToDetail(context, tourism),
+      child: Card.outlined(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
           ),
-          Text(
-            tourism.description,
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
-            softWrap: true,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                tourism.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                tourism.description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                softWrap: true,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  GestureTapCallback _goToDetail(BuildContext context, Tourism tourism) {
+    // TODO: Implement switching pages
+    debugPrint("[DEBUG] Switching page to /detail");
+    return () {
+      Navigator.pushNamed(context, "/detail", arguments: tourism);
+    };
   }
 }
