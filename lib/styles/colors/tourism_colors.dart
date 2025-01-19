6e5807502ee9855ad399bd0d4c6196003c330560
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-01-19 16:37:27 +0700

    feat(themes): :sparkles: added custom theme, enhanced ui
    seed color, define themes and fonts

---
+++ lib/styles/colors/tourism_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+enum TourismColors {
+  blue("Blue", Colors.blue);
+
+  final String name;
+  final Color color;
+  const TourismColors(this.name, this.color);
+}
