bc0f7356dcd27f733a4735adb6bdde89db3b88d9
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-01-19 16:40:23 +0700

    feat(theme): :sparkles: implement custom theme and fonts
    dark mode, light mode, app fonts
    
    BREAKING CHANGE: - assign themes for dark and light mode
    - assign fonts to app via theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:tourism/models/tourism.dart';
 import 'package:tourism/screens/home/home_screen.dart';
 import './screens/detail/detail_screen.dart';
 import 'package:tourism/static/navigation_route.dart';
+import 'styles/themes/tourism_theme.dart';
 
 void main() {
   runApp(App());
@@ -20,7 +21,10 @@ class App extends StatelessWidget {
         NavigationRoute.detailRoute.name: (context) => DetailScreen(
             tourism: ModalRoute.of(context)?.settings.arguments as Tourism),
       },
-      title: 'Flutter Demo',
+      title: 'Tourism App',
+      theme: TourismTheme.lightTheme,
+      darkTheme: TourismTheme.darkTheme,
+      themeMode: ThemeMode.system,
       // theme: ThemeData(
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
