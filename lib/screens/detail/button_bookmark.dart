a2e565e872b9cddd830625a62a9b5bfa0f370de4
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-01-23 01:26:22 +0700

    feat(widget): :sparkles: created bookmark icon
    also added the business logic behind it

---
+++ lib/screens/detail/button_bookmark.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:tourism/models/tourism.dart';
+
+class ButtonBookmark extends StatefulWidget {
+  const ButtonBookmark({super.key, required this.tourism});
+
+  final Tourism tourism;
+
+  @override
+  State<ButtonBookmark> createState() => _ButtonBookmarkState();
+}
+
+class _ButtonBookmarkState extends State<ButtonBookmark> {
+  late bool _isBookmarked;
+
+  @override
+  void initState() {
+    final currentTourism = bookmarkedTourismList
+        .where((current) => current.id == widget.tourism.id);
+
+    // if current tourism is in bookmarked list, light up the bookmark icon
+    setState(() {
+      if (currentTourism.isNotEmpty) {
+        _isBookmarked = true;
+      } else {
+        _isBookmarked = false;
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        setState(_addBookmark);
+      },
+      icon: Icon(_isBookmarked
+          ? Icons.bookmark_rounded
+          : Icons.bookmark_outline_outlined),
+      color: _isBookmarked ? Colors.deepPurpleAccent : Colors.grey,
+    );
+  }
+
+  void _addBookmark() {
+    if (_isBookmarked) {
+      // if already bookmarked, remove ontap
+      bookmarkedTourismList
+          .removeWhere((current) => widget.tourism.id == current.id);
+    } else {
+      // else, if havent already, add ontap
+      bookmarkedTourismList.add(widget.tourism);
+    }
+    _isBookmarked = !_isBookmarked;
+  }
+}
