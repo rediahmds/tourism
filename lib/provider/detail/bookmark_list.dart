f16f09080cd6f2d5dfebd1c02c5fe7c217e16b52
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-01-26 15:41:03 +0700

    feat(state): :sparkles: manage bookmarked list state with provider

---
+++ lib/provider/detail/bookmark_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:tourism/models/tourism.dart';
+
+class BookmarkListProvider extends ChangeNotifier {
+  final List<Tourism> _bookmarkList = [];
+
+  List<Tourism> get bookmarkList => _bookmarkList;
+
+  void addBookmark(Tourism tourism) {
+    _bookmarkList.add(tourism);
+    notifyListeners();
+  }
+
+  void removeBookmark(Tourism tourism) {
+    _bookmarkList.removeWhere((bookmarked) => bookmarked.id == tourism.id);
+    notifyListeners();
+  }
+
+  bool isBookmarked(Tourism tourism) {
+    final bookmarkedList = _bookmarkList.where((bookmarked) => bookmarked.id == tourism.id);
+    return bookmarkedList.isNotEmpty;
+  }
+}
