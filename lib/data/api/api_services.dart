fae1a4f160e4c54b2d00e58461a5074c18b1c7ef
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-02 00:12:41 +0700

    feat(network): :sparkles: fetch detail from api

--- lib/data/api/api_services.dart
+++ lib/data/api/api_services.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:tourism/data/models/tourism_detail_response.dart';
 import 'package:tourism/data/models/tourism_list_response.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,11 +11,25 @@ class APIServices {
     final endpoint = "$_baseURL/list";
     final response = await http.get(Uri.parse(endpoint));
 
-    if (response.statusCode == 200) {
+    if (isResponseOK(response.statusCode)) {
       return TourismListResponse.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("Unable to load tourism list");
     }
   }
 
+  Future<TourismDetailResponse> getTourismDetail(int id) async {
+    final endpoint = "$_baseURL/detail/$id";
+    final response = await http.get(Uri.parse(endpoint));
+
+    if (isResponseOK(response.statusCode)) {
+      return TourismDetailResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Unable to fetch tourism detail with id of $id");
+    }
+  }
+
+  bool isResponseOK(int statusCode) {
+    return statusCode == 200 ? true : false;
+  }
 }
