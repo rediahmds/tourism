c8e7dee0373b715b942e80f7291425211098e7b1
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-01 22:51:03 +0700

    feat(screen): :sparkles: home screen data now comes from api

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tourism/data/api/api_services.dart';
+import 'package:tourism/data/models/tourism_list_response.dart';
 import 'tourism_card.dart';
 import 'package:tourism/data/models/tourism.dart';
 import 'package:tourism/static/navigation_route.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({
     super.key,
   });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<TourismListResponse> _futureTourismReponse;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _futureTourismReponse = APIServices().getTourismList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +30,40 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Tourism List"),
       ),
-      body: ListView.builder(
-        itemCount: tourismList.length,
-        itemBuilder: (context, index) {
-          final tourism = tourismList[index];
+      body: FutureBuilder(
+        future: _futureTourismReponse,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
 
-          return TourismCard(
-              tourism: tourism,
-              onTap: () {
-                Navigator.pushNamed(context, NavigationRoute.detailRoute.name,
-                    arguments: tourism);
-              });
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                final errorMessage = snapshot.error.toString();
+                return Center(
+                  child: Text(errorMessage),
+                );
+              }
+
+              final tourismList = snapshot.data!.places;
+              return ListView.builder(
+                itemCount: tourismList.length,
+                itemBuilder: (context, index) {
+                  final tourism = tourismList[index];
+
+                  return TourismCard(
+                      tourism: tourism,
+                      onTap: () {
+                        Navigator.pushNamed(
+                            context, NavigationRoute.detailRoute.name,
+                            arguments: tourism);
+                      });
+                },
+              );
+
+            default:
+              return const SizedBox();
+          }
         },
       ),
     );
